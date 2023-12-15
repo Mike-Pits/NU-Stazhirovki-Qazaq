94b94fe1082835032b7fd96febf86ed93f407e3b
Mikhail Pitsukov <88615651+Mike-Pits@users.noreply.github.com>
2023-12-15 12:52:51 +0300

    Update test_bar.py

--- Mikhail_Pitsukov/yolov8/test_bar.py
+++ Mikhail_Pitsukov/yolov8/test_bar.py
@@ -1,39 +1,34 @@
 import matplotlib.pyplot as plt
 
-# Sample data
+# Список метрик
 metrics = ['Precision', 'Recall', 'mAP50', 'mAP50-95']
+
+# Список моделей/выборок
 x = ['n_val', 'n_test','s_val', 's_test', 'm_val', 'm_test']
 
-y1 = [0.974, 0.976, 0.986, 0.986, 0.969, 0.982]
-y2 = [0.967, 0.963, 0.977, 0.973, 0.966, 0.949]
-y3 = [0.985, 0.983, 0.99, 0.989, 0.982, 0.98]
-y4 = [0.821, 0.832, 0.852, 0.856, 0.845, 0.853]
+# Списки величин метрик по каждой модели/выборке
+y1 = [0.974, 0.976, 0.986, 0.986, 0.969, 0.982] # Precision
+y2 = [0.967, 0.963, 0.977, 0.973, 0.966, 0.949] # Recall
+y3 = [0.985, 0.983, 0.99, 0.989, 0.982, 0.98] # mAP50
+y4 = [0.821, 0.832, 0.852, 0.856, 0.845, 0.853] # mAP50-95
 
-# Create a figure with four subplots
+# Создаём "канвас" для четырех графиков
 fig, axes = plt.subplots(2, 2, figsize=(10, 8))
 
-# Plot the first subplot
-x_pos = [0,0.75,3,4,6,7]
 axes[0, 0].bar(x, y1)
 axes[0, 0].set_yscale('log')
 axes[0, 0].set_title(metrics[0])
-axes[0, 0].set_xlabel('models size/split')
 axes[0, 0].set_ylabel('%')
-# axes[0, 0].plot(x, y1)
 
-# Plot the second subplot
 axes[0, 1].bar(x, y2)
 axes[0, 1].set_yscale('log')
 axes[0, 1].set_title(metrics[1])
-axes[0, 1].set_xlabel('models size/split')
 axes[0, 1].set_ylabel('%')
 
-# Plot the third subplot
-# Plot the second subplot
+
 axes[1, 0].bar(x, y3)
 axes[1, 0].set_yscale('log')
 axes[1, 0].set_title(metrics[2])
-axes[1, 0].set_xlabel('models size/split')
 axes[1, 0].set_ylabel('%')
 
 # Plot the fourth subplot
@@ -41,12 +36,8 @@ axes[1, 0].set_ylabel('%')
 axes[1, 1].bar(x, y4)
 axes[1, 1].set_yscale('log')
 axes[1, 1].set_title(metrics[3])
-axes[1, 1].set_xlabel('models size/split')
 axes[1, 1].set_ylabel('%')
 
-
-# Adjust the spacing between subplots
 fig.tight_layout()
 
-# Show the figure
 plt.show()
